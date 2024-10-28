30a4a10a5b5372e35b2a0223abeca4cc311bae38
Joel Kwihangana <joelkwijoe@gmail.com>
2024-10-28 05:41:47 +0200

    Improve responsive layout, dark mode support, and image positioning in Header and NavBar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Header from "./components/Header";
 import "./index.css";
+import MainComponent from "./components/MainContent";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Header
-        title="Physiotherapy Portfolio"
-        subtitle="Helping you recover and thrive"
-      />
-    </div>
+    <>
+      {/* Main Container */}
+      <div className="relative min-h-screen bg-gradient-to-t from-primary dark:from-slate-800 dark:to-slate-900 overflow-hidden">
+        <Header
+          title="PhysioEliezel"
+          subtitle="Helping you recover and thrive"
+        />
+        <MainComponent />
+      </div>
+    </>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,21 @@
-import React from "react";
+import headerImage from "../assets/garelly/man.png";
+import NavBar from "./NavBar";
 
 interface HeaderProps {
   title: string;
   subtitle?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ title, subtitle }) => {
+const Header: React.FC<HeaderProps> = () => {
   return (
-    <header className="p-4 bg-gradient-to-r from-primary to-secondary text-white shadow-md">
-      <div className="container mx-auto p-5 flex flex-wrap flex-col md:flex-row items-center">
-        <a
-          href="#"
-          className="flex flex-col font-medium items-center text-white mb-4 md:mb-0"
-        >
-          <span className="text-2xl font-bold">{title}</span>
-          {subtitle && (
-            <span className="text-sm text-gray-200">{subtitle}</span>
-          )}
-        </a>
-
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a href="#services" className="mr-5 hover:text-gray-200">
-            Services
-          </a>
-          <a href="#about" className="mr-5 hover:text-gray-200">
-            About
-          </a>
-          <a href="#testimonials" className="mr-5 hover:text-gray-200">
-            Testimonials
-          </a>
-          <a href="#contact" className="mr-5 hover:text-gray-200">
-            Contact
-          </a>
-        </nav>
-      </div>
-    </header>
+    <>
+      <NavBar />
+      <img
+        src={headerImage}
+        alt="pysioEliezel"
+        className=" absolute bottom-0 right-0 lg:left-0 mx-auto h-5/6 object-cover"
+      />
+    </>
   );
 };
 
