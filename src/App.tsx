63e81393dcdfff838d505ccaad461b8f5aa3eecb
joel kwihangana <joelkwijoe@gmail.com>
2024-11-02 19:26:08 +0200

    fix responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Header from "./components/Header";
 import "./index.css";
-import MainComponent from "./components/MainContent";
+import IntroSection from "./components/IntroSection";
+import Portfolio from "./components/Portfolio";
 
 const App: React.FC = () => {
   return (
     <>
-      {/* Main Container */}
       <Header title="PhysioEliezel" subtitle="Helping you recover and thrive" />
-      <MainComponent />
+      <IntroSection />
+      {/* <AboutSection />
+      <ServicesSection />
+      <WorksSection />
+      <ContactSection /> */}
+      {/* <Portfolio /> */}
     </>
   );
 };
---
+++ src/components/AboutSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutSection: React.FC = () => {
+  return <div></div>;
+};
+
+export default AboutSection;
